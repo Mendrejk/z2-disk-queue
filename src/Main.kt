12e191877816797de7aa893747beff1f489e067b
Mendrejk <bastian.lakomy@gmail.com>
2020-04-28 11:27:08 +0200

    Formatted printed simulation outcome.

--- src/Main.kt
+++ src/Main.kt
@@ -6,10 +6,12 @@ fun main() {
     val maxExpirationTime: Int = 100
     val requests = generateRequests(howMany, maxAppearanceTime, diskSize, realTimePercentage, maxExpirationTime)
 
-    println(firstComeFirstServe(diskSize, requests.sortedBy { it.appearanceTime }.toMutableList()))
-    println(shortestSeekTimeFirst(diskSize, requests.sortedBy { it.appearanceTime }.toMutableList()))
-    println(cScan(diskSize, requests.sortedBy { it.appearanceTime }.toMutableList()))
-    println(scan(diskSize, requests.sortedBy { it.appearanceTime }.toMutableList()))
-    println(earliestDeadlineFirst(diskSize, requests.sortedBy { it.appearanceTime }.toMutableList()))
-    println(feasibleDeadlineScan(diskSize, requests.sortedBy { it.appearanceTime }.toMutableList()))
+    println("fcfs total path: ${firstComeFirstServe(diskSize, requests.sortedBy { it.appearanceTime }.toMutableList())}")
+    println("sstf total path: ${shortestSeekTimeFirst(diskSize, requests.sortedBy { it.appearanceTime }.toMutableList())}")
+    println("c-scan total path: ${cScan(diskSize, requests.sortedBy { it.appearanceTime }.toMutableList())}")
+    println("scan total path: ${scan(diskSize, requests.sortedBy { it.appearanceTime }.toMutableList())}")
+    val edfResult: Pair<Int, Double> = earliestDeadlineFirst(diskSize, requests.sortedBy { it.appearanceTime }.toMutableList())
+    println("edf total path: ${edfResult.first}, real-time task fail percentage: ${edfResult.second * 100}%")
+    val fdScanResult: Pair<Int, Double> = feasibleDeadlineScan(diskSize, requests.sortedBy { it.appearanceTime }.toMutableList())
+    println("fd-scan total path: ${fdScanResult.first}, real-time task fail percentage: ${fdScanResult.second * 100}%")
 }
